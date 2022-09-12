221aafdfc35cfd69b963ad814b377b91a6e9475d
guitardev <41134396+guitardev@users.noreply.github.com>
2022-09-12 16:42:33 +0700

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import stylesheets
 import './style.css';
 
+// import @line/liff
+import liff from '@line/liff';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
